87034c9bd67915cc21666f11b6a77d5a557c4430
BryanZhong <122657816+BryanZhong@users.noreply.github.com>
2024-03-11 10:51:40 +0800

    0.0.6
    CLI debug

--- src/NumberleGameForCLI.java
+++ src/NumberleGameForCLI.java
@@ -31,16 +31,15 @@ public class NumberleGameForCLI {
 
 
             while (attempts <= 5) {
-                System.out.println("-".repeat(20) + "当前输入：" + (attempts <= 5) + "-".repeat(20));
                 System.out.println("当前输入：" + model.listToString(playerInput));
                 System.out.println("请输入单个字符继续编辑或输入'd'删除最后一个值：");
                 String input = scanner.next();
 
                 // 输入处理
                 model.handleInput(input, playerInput);
-
                 // 提交并对比
                 if (playerInput.size() == model.MAX_LENGTH) {
+                    System.out.println("当前输入：" + model.listToString(playerInput) + "。是否提交此答案? (y/n)");
                     String decision = scanner.next();
                     if ("y".equalsIgnoreCase(decision)) {
                         if (model.isValidEquation(model.listToString(playerInput))) {
